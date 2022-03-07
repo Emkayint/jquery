b8ece828a23d3c24178e3d04c54bfe522cd540fc
Martin <mkmartinoes@gmail.com>
2022-03-07 19:40:55 +0300

    getting data with jquery

--- JS/script.js
+++ JS/script.js
@@ -4,19 +4,32 @@
 //     });
 // });
 
+// $(document).ready(() => {
+//     $("button#hello").click(() => {
+//         $("ul#user").prepend("<li>Hello</li>")
+//         $('ul#browser').prepend('<li>Why Hello </li>')
+//         $('li').css('background-color', 'green')
+//         $('ul#user').children('li').first().click( function () {
+//             $(this).remove();
+//         })
+//     });
+//     $("button#goodbye").click(() => {
+//         $("ul").prepend("<li>Goodbye</li>")
+//     });
+//     $("button#stop").click(() => {
+//         $("ul").prepend("<li>Stop copying me</li>")
+//     });
+// });
+
 $(document).ready(() => {
-    $("button#hello").click(() => {
-        $("ul#user").prepend("<li>Hello</li>")
-        $('ul#browser').prepend('<li>Why Hello </li>')
-        $('li').css('background-color', 'green')
-        $('ul#user').children('li').first().click( function () {
-            $(this).remove();
-        })
-    });
-    $("button#goodbye").click(() => {
-        $("ul").prepend("<li>Goodbye</li>")
-    });
-    $("button#stop").click(() => {
-        $("ul").prepend("<li>Stop copying me</li>")
-    });
-});
+    $("#blanks form").submit(() => {
+        $(".person1").append('blah blah');
+        $(".person2").append('blah blah');
+        $(".animal").append('blah blah');
+        $("exclamation").append('blah blah');
+        $(".verb").append('blah blah');
+        $(".noun").append('blah blah');
+
+        $("#story").show();
+    })
+})
