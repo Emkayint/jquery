8cdc6b997d0ba77cc8eace0fa870b9a90e2d4979
Martin <mkmartinoes@gmail.com>
2022-03-08 07:03:35 +0300

    getting form data and displaying it

--- JS/script.js
+++ JS/script.js
@@ -21,16 +21,37 @@
 //     });
 // });
 
+// $(document).ready(() => {
+//     $("#blanks form").submit((event) => {
+//         $(".person1").append('blah blah');
+//         $(".person2").append('blah blah');
+//         $(".animal").append('blah blah');
+//         $(".exclamation").append('blah blah');
+//         $(".verb").append('blah blah');
+//         $(".noun").append('blah blah');
+
+//         $("#story").show();
+//         event.preventDefault();
+//     })
+// })
+
 $(document).ready(() => {
     $("#blanks form").submit((event) => {
-        $(".person1").append('blah blah');
-        $(".person2").append('blah blah');
-        $(".animal").append('blah blah');
-        $(".exclamation").append('blah blah');
-        $(".verb").append('blah blah');
-        $(".noun").append('blah blah');
+        let person1Input = $("input#person1").val();
+        let person2Input = $("input#person2").val();
+        let aniamlInput = $("input#animal").val();
+        let exclamationInput = $("input#exclamation").val();
+        let verbInput = $("input#verb").val();
+        let nounInput = $("input#noun").val();
 
-        $("#story").show();
+        $('.person1').text(person1Input);
+        $('.person2').text(person2Input);
+        $('.animal').text(aniamlInput);
+        $('.exclamation').text(exclamationInput);
+        $('.verb').text(verbInput);
+        $('.noun').text(nounInput);
+
+        $('#story').show();
         event.preventDefault();
-    })
-})
+    });
+});
